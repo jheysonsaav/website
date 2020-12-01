0e98ec986f2730f0bef63d3561d324f8e43858a0
JheysonDev <JheysonDev@outlook.com>
2020-12-01 12:08:21 -0500

    Eliminando la maldita basura de google analytics XD

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import VueGtag from "vue-gtag";
-
-App.use(VueGtag, {
-  config: { id: "G-ZXJBVFVGEZ" }
-});
 
 createApp(App)
   .use(router)
