b326308be2266a5cb7f43ad80da9bff0251a0b7d
JheysonDev <JheysonDev@outlook.com>
2020-12-01 12:00:34 -0500

    Reparando google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueGtag from "vue-gtag";
+
+App.use(VueGtag, {
+  config: { id: "G-ZXJBVFVGEZ" }
+});
 
 createApp(App)
   .use(router)
