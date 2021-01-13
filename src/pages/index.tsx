94446c434d4e5d8253dacf7b56d1c24d0bd4e545
JheysonDev <JheysonDev@outlook.com>
2021-01-13 16:02:44 -0500

    fix: refactor to widht in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 // Copyright (C) Jheyson Saavedra ~ All right reserved.
 import Layout from 'src/components/Layout';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 
 function Home(): JSX.Element {
-  const { query } = useRouter();
-
   return (
     <Layout header title="">
       <div className="m-3 inline-block text-center md:flex md:justify-evenly md:items-center md:my-8">
@@ -22,7 +19,6 @@ function Home(): JSX.Element {
           <span className="text-primary-500 text-md font-semibold">
             Software Developer
           </span>
-          <span>{query.asd}</span>
           <p className="text-gray-700 dark:text-gray-300 pt-3 max-w-md">
             I am a coffee lover fullstack programmer with experience in
             technologies such as javascript, typescript, react, vue, go etc.
