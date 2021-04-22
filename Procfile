1a24f8e9eddcdcd46d7c2511a4bd56e1b70f5f85
Jheyson Saavedra <jheysonsaav@users.noreply.github.com>
2021-04-21 21:33:54 -0500

    feat: add heroku files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: deno run --allow-all https://deno.land/x/aleph@v0.3.0-alpha.31/cli.ts start --port=$PORT
---
+++ heroku_build.ts
@@ -0,0 +1,33 @@
+import { moveSync } from "https://deno.land/std@0.94.0/fs/mod.ts";
+
+const text = await Deno.readTextFile("import_map.json");
+const alephPath = JSON.parse(text).imports["aleph/"];
+
+const buildStatus = await Deno.run({
+  cmd: [
+    Deno.execPath(),
+    "run",
+    "--allow-all",
+    `${alephPath}cli.ts`,
+    "build",
+  ],
+  stdout: "inherit",
+  stderr: "inherit",
+}).status();
+
+console.log(`Build Exit Code: ${buildStatus.code}`);
+
+const startStatus = await Deno.run({
+  cmd: [
+    Deno.execPath(),
+    "run",
+    "--allow-net=deno.land,esm.sh,cdn.esm.sh",
+    `${alephPath}cli.ts`,
+    "start",
+    "--help",
+  ],
+  stdout: "null",
+  stderr: "inherit",
+}).status();
+
+console.log(`Help Exit Code: ${startStatus.code}`);
